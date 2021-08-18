e2ac295a6ee240c74dfff8816098a7098446a8c8
Mahabubul Hasan <shourav6688@gmail.com>
2021-08-18 19:59:24 +0600

    nav changed

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -16,18 +16,12 @@
     <div class="navbar-end">
       <div class="navbar-item">
         <div class="buttons" v-if="($route.name === 'Register') || ($route.name === 'Login')">
-          <!-- <a class="button is-info" @click="handlePushToCreateNote">
-            Inscribe
-          </a> -->
           <a class="button is-primary" @click="handlePushToRegister">
             <strong>Register</strong>
           </a>
           <a class="button is-light" @click="handlePushToLogin">
             Log in
           </a>
-          <!-- <a class="button is-danger" @click="handleLogout">
-            Log Out
-          </a> -->
         </div>
 
 
@@ -35,12 +29,6 @@
           <a class="button is-info" @click="handlePushToCreateNote">
             Inscribe
           </a>
-          <!-- <a class="button is-primary" @click="handlePushToRegister">
-            <strong>Register</strong>
-          </a>
-          <a class="button is-light" @click="handlePushToLogin">
-            Log in
-          </a> -->
           <a class="button is-danger" @click="handleLogout">
             Log Out
           </a>
@@ -67,28 +55,7 @@ export default {
         const route = useRoute();
         const state = reactive({
           selected: null,
-          // user: []
         });
-
-
-      //   watch(() => route.name, () => {
-      //   if(route.name === 'CreateNote' || route.name === 'Home') {
-      //     state.selected = 0;
-      //      console.log(route.name);
-      //   } else if (route.name === 'Login' || route.name === 'Register')  {
-      //     state.selected = 1;
-      //     console.log(route.name);
-      //   }
-      // })
-
-      // const showNavContent = () =>{
-      //   const route_name = route.name
-      //   console.log(route_name);
-      //   return route_name
-      // }
-
-
-
 
 
         const handlePushToHome = _ => router.push({ name: 'Home' });
@@ -103,46 +70,6 @@ export default {
         }
 
 
-    //   const getUser = async (_) => {
-    //   try {
-    //     const response = await axios.get(`${backend}/api/get_user`, {
-    //       headers: {
-    //         Authorization: `Bearer ${localStorage.accessToken}`,
-    //       },
-    //     });
-    //     if (response.status === 200) {
-    //       state.user = response.data;
-    //       console.log(state.user)
-    //     }
-    //   } catch (err) {
-    //     if (err.status === 500) {
-    //       //Throw error
-    //     } else {
-    //       try {
-    //         const refresh = await axios.post(`${backend}/api/token/refresh/`, {
-    //           refresh: localStorage.refreshToken,
-    //         });
-    //         localStorage.setItem("accessToken", refresh.data.access);
-    //         const refreshResponse = await axios.get(`${backend}/api/get_user`, {
-    //           headers: {
-    //             Authorization: `Bearer ${refresh.data.access}`,
-    //           },
-    //         });
-    //         if (refreshResponse.status === 200) {
-    //           state.user = refreshResponse.data;
-    //         }
-    //       } catch {
-    //         localStorage.removeItem("accessToken");
-    //         localStorage.removeItem("refreshToken");
-    //         await router.push({ name: "Login" });
-    //       }
-    //     }
-    //   }
-    // };
-    // onMounted(() => {
-    //   getUser();
-    // });
-
 
         return {
             handlePushToRegister,
@@ -151,7 +78,6 @@ export default {
             handlePushToHome,
             handlePushToCreateNote,
             handlePushToShare
-            // showNavContent
         }
     }
 }
